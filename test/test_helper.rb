8aea73a57b37a0501ef64799b2cb5bd80727ab3c
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2024-03-16 11:13:04 -0400

    Change the way we require mocha/minitest

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,7 @@ require 'simplecov'
 require 'minitest/autorun'
 #require 'shoulda'
 require 'test_construct'
-require 'mocha/mini_test'
+require 'mocha/minitest'
 
 SimpleCov.start do
   add_filter 'specs/ruby/1.9.1/gems/'
@@ -14,7 +14,7 @@ end
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'churn/calculator'
-Mocha::Configuration.prevent(:stubbing_non_existent_method)
+# Mocha::Configuration.prevent(:stubbing_non_existent_method)
 
 class Minitest::Test
   include TestConstruct::Helpers
