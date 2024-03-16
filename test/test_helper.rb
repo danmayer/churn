5b6b327f264d2423c1f9628ee8d54443e6b2430e
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2024-03-16 11:19:29 -0400

    This call is no longer needed

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,7 +14,6 @@ end
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'churn/calculator'
-# Mocha::Configuration.prevent(:stubbing_non_existent_method)
 
 class Minitest::Test
   include TestConstruct::Helpers
